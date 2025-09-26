4c15eeef64d6bba427199fd3a456aa2b8fe858d8
Lucas Henrique de Sousa Pinto <xekrs2000@gmail.com>
2025-09-26 10:23:37 +0200

    Added file url and name to messageSchema

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   text: String,
   sender: String,
+  fileUrl: String,
+  fileName: String,
   timestamp: {
     type: Date,
     default: Date.now
